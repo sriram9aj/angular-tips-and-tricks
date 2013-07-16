c586d9694a77671ae2dde54458ecfea48d62442e
Butch Peters <brian@3snip.com>
2013-07-16 15:12:56 -0500

    Fixed indenting and width

--- Promises/app.js
+++ Promises/app.js
@@ -1,109 +1,117 @@
 angular.module("app", [])
 
 .service("UserInput", function ($rootScope, $q) {
-    return {
-        prompt: function (successData, failureReason, title) {
-            var deferred = $q.defer();
-            $rootScope.$broadcast('promptUser', deferred, successData, failureReason, title || 'Choose one...');
-            return deferred.promise;
-        },
+  return {
+    prompt: function (successData, failureReason, title) {
+      var deferred = $q.defer();
+      $rootScope.$broadcast('promptUser', deferred, 
+        successData, failureReason, 
+        title || 'Choose one...');
+      return deferred.promise;
+    },
 
-        promptAndIncreaseExcitement: function (successData, failureReason) {
-            function increaseSuccess(data) {
-                return data + "!";
-            }
+    promptAndIncreaseExcitement: function (successData, failureReason) {
+      function increaseSuccess(data) {
+        return data + "!";
+      }
 
-            function increaseFailure(reason) {
-                return $q.reject(reason + "!");
-            }
+      function increaseFailure(reason) {
+        return $q.reject(reason + "!");
+      }
 
-            return this.prompt(successData, failureReason).then(increaseSuccess, increaseFailure);
-        },
+      return this.prompt(successData, failureReason)
+                 .then(increaseSuccess, increaseFailure);
+    },
 
-        promptAndHideRejection: function (successData, failureReason) {
-            function hideRejection(reason) {
-                return successData + ' (' + reason + ')';
-            }
+    promptAndHideRejection: function (successData, failureReason) {
+      function hideRejection(reason) {
+        return successData + ' (' + reason + ')';
+      }
 
-            return this.prompt(successData, failureReason).then(null, hideRejection);
-        },
+      return this.prompt(successData, failureReason)
+                 .then(null, hideRejection);
+    },
 
-        promptAndConfirm: function (successData, failureReason) {
-            var self = this;
+    promptAndConfirm: function (successData, failureReason) {
+      var self = this;
 
-            function confirm(originalChoice) {
-                return self.prompt(originalChoice + ' confirmed', originalChoice + ' rejected', 'Confirm your choice...');
-            }
+      function confirm(originalChoice) {
+        return self.prompt(originalChoice + ' confirmed', 
+          originalChoice + ' rejected', 
+          'Confirm your choice...');
+      }
 
-            return this.prompt(successData, failureReason).then(confirm, confirm);
-        },
+      return this.prompt(successData, failureReason)
+                 .then(confirm, confirm);
+    },
 
-        bypassPromptAndReturnImmediately: function (successData) {
-            return successData;
-        }
+    bypassPromptAndReturnImmediately: function (successData) {
+        return successData;
     }
+  }
 })
 
 .controller("PromiseResolver", function ($scope) {
-    $scope.resolveIt = function () {
-        $scope.deferred.resolve($scope.successData);
-        delete $scope.deferred;
-    };
+  $scope.resolveIt = function () {
+    $scope.deferred.resolve($scope.successData);
+    delete $scope.deferred;
+  };
 
-    $scope.rejectIt = function () {
-        $scope.deferred.reject($scope.failureReason);
-        delete $scope.deferred;
-    };
+  $scope.rejectIt = function () {
+    $scope.deferred.reject($scope.failureReason);
+    delete $scope.deferred;
+  };
 
-    $scope.$on('promptUser', function (event, deferred, successData, failureReason, title) {
-        $scope.deferred = deferred;
-        $scope.successData = successData;
-        $scope.failureReason = failureReason;
-        $scope.title = title;
-    });
+  $scope.$on('promptUser', function (event, deferred, successData, failureReason, title) {
+    $scope.deferred = deferred;
+    $scope.successData = successData;
+    $scope.failureReason = failureReason;
+    $scope.title = title;
+  });
 })
 
 .controller('PromiseExamples', function ($scope, UserInput) {
-    $scope.UserInput = UserInput;
+  $scope.UserInput = UserInput;
 })
 
 .directive('prompt', function ($q) {
-    return {
-        restrict: 'EA',
-        templateUrl: '/prompt.html',
-        scope: {
-            action: '@',
-            flexible: '='
-        },
-        controller: function ($scope) {
-            function callAction() {
-                return $scope.$parent.$eval($scope.action);
-            }
+  return {
+    restrict: 'EA',
+    templateUrl: '/prompt.html',
+    scope: {
+      action: '@',
+      flexible: '='
+    },
+    controller: function ($scope) {
+      function callAction() {
+        return $scope.$parent.$eval($scope.action);
+      }
 
-            function success(data) {
-                delete $scope.prompted;
-                return 'Promise resolved: ' + data;
-            }
+      function success(data) {
+        delete $scope.prompted;
+        return 'Promise resolved: ' + data;
+      }
 
-            function failure(reason) {
-                delete $scope.prompted;
-                return 'Promise rejected: ' + reason;
-            }
+      function failure(reason) {
+        delete $scope.prompted;
+        return 'Promise rejected: ' + reason;
+      }
 
-            $scope.promptUser = function () {
-                $scope.result = callAction().then(success, failure);
-                $scope.prompted = true;
-            }
+      $scope.promptUser = function () {
+        $scope.result = callAction().then(success, failure);
+        $scope.prompted = true;
+      }
 
-            $scope.promptUserFlexible = function () {
-                $scope.result = $q.when(callAction()).then(success, failure);
-                $scope.prompted = true;
-            }
+      $scope.promptUserFlexible = function () {
+        $scope.result = $q.when(callAction())
+                          .then(success, failure);
+        $scope.prompted = true;
+      }
 
-            $scope.promptFunction = "promptUser()";
-            if ($scope.flexible) {
-                $scope.promptFunction = "promptUserFlexible()";
-            }
-        }
+      $scope.promptFunction = "promptUser()";
+      if ($scope.flexible) {
+        $scope.promptFunction = "promptUserFlexible()";
+      }
     }
+  }
 });
